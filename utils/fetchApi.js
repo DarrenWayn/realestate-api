f8a4edded29f99393ceaccd507b76768db0210d6
Darren Wayn <darrenwayn98@gmail.com>
2022-05-15 22:34:11 +0700

    Update fetchApi.js
    added api

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async(url) => {
     const { data } = await axios.get((url), {
         headers: {
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-            'X-RapidAPI-Key': ''
+            'X-RapidAPI-Key': 'ae9c37b302mshf18f6bdeabd3a3cp131c7cjsnec96c2f702e9'
             // enter your own api key
           }
     })
